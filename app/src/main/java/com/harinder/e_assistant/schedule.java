d5962e3343d2bb98855965e522130dccb23e2b9b
Harinder Pal Singh <harinder2612@gmail.com>
2017-10-19 02:47:09 +0530

    Initial commit

---
+++ app/src/main/java/com/harinder/e_assistant/schedule.java
@@ -0,0 +1,13 @@
+package com.harinder.e_assistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class schedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
